eb4f4cb8f9626e12a4619b9f37e1b5f179b388e8
Cristiano7988 <cristiano.morales@contractor.outroll.com>
2022-09-19 11:30:32 -0300

    ✨ Creating the car's list

---
+++ src/components/carList/CarList.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class AppButton extends Component {
+  constructor() {
+    super();
+    this.state = {
+        cars: []
+    }
+  }
+  componentDidMount() {
+    this.getCars("cars");
+  }
+  getCars(endpoint) {
+    fetch(`http://api-test.bhut.com.br:3000/api/${endpoint}`)
+      .then((r) => (r.ok ? r.json() : false))
+      .then((r) => this.setState({cars: r}))
+      .catch((e) => console.log("Error", e));
+  }
+  render() {
+    return <div>
+        {this.state.cars.map(({_id, title, brand, price, age}) => {
+            return <ul key={_id}>
+              <li><b>{title}</b></li>
+              <li>Marca: {brand}</li>
+              <li>Ano: {age}</li>
+              <li>Preço: {price}</li>
+            </ul>
+        })}
+    </div>;
+  }
+}
+
+export default AppButton;
